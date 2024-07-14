0f962bca9b155cce32d88b65ffb0e9cad0d18dba
Yasir Aquil <yasiraquil121@gmail.com>
2024-07-14 13:39:02 +0530

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+io.on("connection", (socket) => {
+    console.log("New WebSocket connection");
+
+    socket.on("send-location", (data) => {
+        io.emit("recieve-location", { id: socket.id, ...data });
+    });
+
+    socket.on("disconnect", () => {
+        io.emit("user-disconnect", socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
